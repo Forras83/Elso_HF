524f018b67611a7a97a8075a91159af344471166
Forrás László <forras25@gmail.com>
2025-09-14 15:43:57 +0200

    Neptun nev

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Ez az első módositás
+# FL WN0JPF  készítette DUE Hallagató
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
